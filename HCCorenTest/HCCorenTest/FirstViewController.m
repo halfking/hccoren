1c10c60d223adc79eaeb4de4e0ad8d1f94e4f9a3
halfking <kimmy.huang@gmail.com>
2016-06-07 00:16:09 +0800

    fix get filename function

--- HCCorenTest/HCCorenTest/FirstViewController.m
+++ HCCorenTest/HCCorenTest/FirstViewController.m
@@ -12,6 +12,7 @@
 #import "base.h"
 #import "CMDS.h"
 #import "HCCallbackresult.h"
+#import "HCFileManager.h"
 @interface FirstViewController ()
 
 @end
@@ -23,6 +24,11 @@
     // Do any additional setup after loading the view, typically from a nib.
     
 //    UIButton * btn = [UIButton alloc]initWithFrame:CGRectMake(50, 50, <#CGFloat width#>, <#CGFloat height#>)
+    
+    HCFileManager * manager = [HCFileManager manager];
+    NSString * fileName = [manager getFileName:@"/Users/huangxutao/Library/Developer/CoreSimulator/Devices/B299442B-51B6-46FE-86AF-B508537ADDA5/data/Containers/Bundle/Application/9A005E17-2BDF-48B3-BC4E-F151D5D75EDA/HCMVManagerTest.app/test2.MOV"];
+    NSLog(@"fileName:%@",fileName);
+    
 }
 - (void)viewDidDisappear:(BOOL)animated
 {
